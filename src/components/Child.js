739fd16ecbde612673409ff02878f1293bce2ad5
vamandeshmukh <vamandeshmukh@gmail.com>
2022-04-15 17:38:30 +0530

    props

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,16 +1,40 @@
 
 const Child = (props) => {
 
-    const pData = props.pDataToPass;
-    const pN = props.p2;
+    const childDataInChild = 15;
+    const parentDataInChild = props.passDataToChild;
+
+    const passChildData = () => {
+        console.log(`child`);
+        props.fun(childDataInChild);
+    }
+
     return (
 
         <div className="container">
             <p>Child Component</p>
-            <p>Parent data in child component: {pData} </p>
-            <p>{pN}</p>
+            <p>Child data in child component: {childDataInChild} </p>
+            <p>Parent data in child component: {parentDataInChild} </p>
+            <button type="button" className="btn btn-primary" onClick={passChildData}>Click</button>
         </div>
 
     );
 }
 export default Child;
+
+
+// const Child = (props) => {
+
+//     const pData = props.pDataToPass;
+//     const pN = props.p2;
+//     return (
+
+//         <div className="container">
+//             <p>Child Component</p>
+//             <p>Parent data in child component: {pData} </p>
+//             <p>{pN}</p>
+//         </div>
+
+//     );
+// }
+// export default Child;
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -2,23 +2,51 @@ import Child from "./Child";
 
 const Parent = () => {
 
-    const parentData = 10;
-    const parentName = "abc";
+    const parentDataInParent = 10;
+    let childDataInParent; // state 
 
-    fun = () => {
-        // code 
+    const getChildData = (num) => {
+        console.log(num); // 15
+        childDataInParent = num;
     }
 
     return (
 
         <div className="container">
             <p>Parent Component</p>
-            <p> Parent data in parent component: {parentData}</p>
-            <Child pDataToPass={parentData}
-                p2={parentName}
-                p3={fun} />
+            <p> Parent data in parent component: {parentDataInParent}</p>
+            <p> Child data in parent component: {childDataInParent}</p>
+            <Child passDataToChild={parentDataInParent}
+                fun={getChildData}
+            />
         </div>
 
     );
 }
 export default Parent;
+
+
+// import Child from "./Child";
+
+// const Parent = () => {
+
+//     const parentData = 10;
+//     const parentName = "abc";
+
+//     fun = () => {
+//         // code
+//     }
+
+//     return (
+
+//         <div className="container">
+//             <p>Parent Component</p>
+//             <p> Parent data in parent component: {parentData}</p>
+//             <Child pDataToPass={parentData}
+//                 p2={parentName}
+//                 p3={fun} />
+//         </div>
+
+//     );
+// }
+// export default Parent;
