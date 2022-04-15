bd79691184151854fed880776336a31520a0ee2b
vamandeshmukh <vamandeshmukh@gmail.com>
2022-04-15 18:57:10 +0530

    useState

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,16 +1,18 @@
+
 import { useState } from "react";
 import Child from "./Child";
+
 
 const Parent = () => {
 
     const parentDataInParent = 10;
     // let childDataInParent; // state 
-    let [childDataInParent, setChildDataInParent] = useState(0);
+    let [childDataInParent, abc] = useState(0);
 
     const getChildData = (num) => {
         console.log(num); // 15
         // childDataInParent = num;
-        setChildDataInParent(num);
+        abc(num);
     }
 
     return (
