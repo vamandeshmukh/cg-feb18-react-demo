b5a48727cec40de19a81a0eb304a907ad76eaeb5
vamandeshmukh <vamandeshmukh@gmail.com>
2022-04-21 18:11:16 +0530

    redux

--- src/components/Page404.js
+++ src/components/Page404.js
@@ -1,18 +1,33 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Redirect } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 const Page404 = () => {
 
     const [shouldRedirect, setShouldRedirect] = useState(false);
+    const [seconds, setSeconds] = useState(5);
+    const timer = useRef();
 
     useEffect(() => {
 
-        setTimeout(() => {
+        const tick = () => {
+            setSeconds(prevSeconds => prevSeconds - 1);
+        }
+
+        timer.current = setInterval(() => tick(), 1000);
+
+    },
+        []);
+
+    useEffect(() => {
+
+        if (seconds === 0) {
             setShouldRedirect(true);
-        }, 3000); 
-    }, 
-    []);
+            clearInterval(timer.current);
+        }
+
+    }, [seconds]);
+
 
     if (shouldRedirect) {
         return (
@@ -24,7 +39,7 @@ const Page404 = () => {
             <div className="container">
                 <h1>Oops!</h1>
                 <p>The page you are looking for is not found.</p>
-                <p>redirecting to home...</p>
+                <p>Redirecting to home in {seconds} seconds. </p>
                 <Link className="btn btn-primary" to="/home" >Let's go home now.</Link>
             </div>
         );
